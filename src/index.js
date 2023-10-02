2794c3b4e896893df3a08fae6c13661113723017
ryabad <ryabaDa@gmail.com>
2023-10-02 17:36:17 +0300

    new variant

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import { fetchBreeds, fetchCatByBreed } from './js/cat-api';
-import axios from 'axios';
+import Notiflix from 'notiflix';
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
 
-axios.defaults.headers.common['x-api-key'] =
-  'live_iFHHuD1PAYpjNu6BjBSizo3AxPvPCEAgps2RkZ0f7KxsmBZTcQNFzQ7OysiNQzMN';
+Notiflix.Notify.init({
+  width: '500px',
+  position: 'center-top',
+});
 
 const refs = {
   select: document.querySelector('.breed-select'),
@@ -11,7 +15,43 @@ const refs = {
   catInfo: document.querySelector('div.cat-info'),
 };
 
-fetchBreeds();
+fetchBreeds()
+  .then(response => {
+    refs.loader.classList.add('visually-hidden');
+    refs.select.classList.remove('visually-hidden');
+    const data = response.data;
+
+    // __________________first variant_____________________
+    const markup = data
+      .map(({ name, id }) => `<option value="${id}">${name}</option>`)
+      .join('');
+
+    refs.select.insertAdjacentHTML('beforeend', markup);
+    new SlimSelect({
+      select: '.breed-select',
+    });
+
+    //__________________second variant____________________
+    // const option = data.map(({ name, id }) => {
+    //   return {
+    //     text: name,
+    //     value: id,
+    //   };
+    // });
+    // new SlimSelect({
+    //   select: '.breed-select',
+    //   data: option,
+    // });
+    //_____________________________________________________
+  })
+  .catch(error => {
+    refs.loader.classList.add('visually-hidden');
+    // refs.error.classList.remove('visually-hidden');
+    Notiflix.Notify.failure(
+      'Oops! Something went wrong! Try reloading the page!'
+    );
+    console.log(error);
+  });
 
 refs.select.addEventListener('change', handleOptionClick);
 
@@ -19,5 +59,43 @@ function handleOptionClick() {
   refs.loader.classList.remove('visually-hidden');
   // refs.error.classList.add('visually-hidden');
   const optionId = refs.select.value;
-  fetchCatByBreed(optionId);
+  fetchCatByBreed(optionId)
+    .then(response => {
+      refs.loader.classList.add('visually-hidden');
+      createMarkup(response.data);
+      // const data = response.data;
+    })
+    .catch(error => {
+      refs.loader.classList.add('visually-hidden');
+      // refs.error.classList.remove('visually-hidden');
+      Notiflix.Notify.failure(
+        'Oops! Something went wrong! Try reloading the page!'
+      );
+      console.log(error);
+    });
+}
+
+function createMarkup(data) {
+  const catInfo = data[0];
+  const { url, breeds } = catInfo;
+  const breed = breeds[0];
+  const { name, description, origin, temperament, wikipedia_url, alt_names } =
+    breed;
+
+  const markup = `
+        <div class="cat-container">
+          <img src="${url}" alt="${alt_names} photo"/>
+          <div class="info-container">
+            <h1>${name}</h1>
+            <h2>About cat!</h2>
+              <p>${description}</p>
+            <h2>Origin Country</h2>
+              <p>${origin}</p>
+            <h2>Temperament</h2>
+              <p>${temperament}</p>
+            <p><a href="${wikipedia_url}" target="_blank">More about ${name}!</a></p>
+          </div>
+        </div>
+      `;
+  refs.catInfo.innerHTML = markup;
 }
--- src/js/cat-api.js
+++ src/js/cat-api.js
@@ -1,106 +1,19 @@
 import axios from 'axios';
-import Notiflix from 'notiflix';
-import SlimSelect from 'slim-select';
-import 'slim-select/dist/slimselect.css';
 
 axios.defaults.headers.common['x-api-key'] =
   'live_iFHHuD1PAYpjNu6BjBSizo3AxPvPCEAgps2RkZ0f7KxsmBZTcQNFzQ7OysiNQzMN';
 
-Notiflix.Notify.init({
-  width: '500px',
-  position: 'center-top',
-});
-
 const refs = {
-  breedOption: document.querySelector('.breed-select'),
   infoCatBlock: document.querySelector('div.cat-info'),
-  loader: document.querySelector('.loader'),
-  // error: document.querySelector('p.error'),
 };
 
 export function fetchBreeds() {
-  return axios
-    .get('https://api.thecatapi.com/v1/breeds')
-    .then(response => {
-      refs.loader.classList.add('visually-hidden');
-      refs.breedOption.classList.remove('visually-hidden');
-      const data = response.data;
-
-      // __________________first variant_____________________
-      const markup = data
-        .map(({ name, id }) => `<option value="${id}">${name}</option>`)
-        .join('');
-
-      refs.breedOption.insertAdjacentHTML('beforeend', markup);
-      new SlimSelect({
-        select: '.breed-select',
-      });
-
-      //__________________second variant____________________
-      // const option = data.map(({ name, id }) => {
-      //   return {
-      //     text: name,
-      //     value: id,
-      //   };
-      // });
-      // new SlimSelect({
-      //   select: '.breed-select',
-      //   data: option,
-      // });
-      //_____________________________________________________
-    })
-    .catch(error => {
-      refs.loader.classList.add('visually-hidden');
-      // refs.error.classList.remove('visually-hidden');
-      Notiflix.Notify.failure(
-        'Oops! Something went wrong! Try reloading the page!'
-      );
-      console.log(error);
-    });
+  return axios.get('https://api.thecatapi.com/v1/breeds');
 }
 
 export function fetchCatByBreed(breedID) {
   refs.infoCatBlock.textContent = '';
-  return axios
-    .get(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedID}`)
-    .then(response => {
-      refs.loader.classList.add('visually-hidden');
-      const data = response.data;
-      const catInfo = data[0];
-      const { url, breeds } = catInfo;
-      const breed = breeds[0];
-      const {
-        name,
-        description,
-        origin,
-        temperament,
-        wikipedia_url,
-        alt_names,
-      } = breed;
-
-      const markup = `
-        <div class="cat-container">
-          <img src="${url}" alt="${alt_names} photo"/>
-          <div class="info-container">
-            <h1>${name}</h1>
-            <h2>About cat!</h2>
-              <p>${description}</p>
-            <h2>Origin Country</h2>
-              <p>${origin}</p>
-            <h2>Temperament</h2>
-              <p>${temperament}</p>
-            <p><a href="${wikipedia_url}" target="_blank">More about ${name}!</a></p>
-          </div>
-        </div>
-      `;
-      refs.infoCatBlock.innerHTML = markup;
-    })
-    .catch(error => {
-      refs.loader.classList.add('visually-hidden');
-      // refs.error.classList.remove('visually-hidden');
-      Notiflix.Notify.failure(
-        'Oops! Something went wrong! Try reloading the page!'
-      );
-      console.log(error);
-    });
+  return axios.get(
+    `https://api.thecatapi.com/v1/images/search?breed_ids=${breedID}`
+  );
 }
